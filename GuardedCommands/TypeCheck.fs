200ad14749676af092bdbacb1176d06e611fc72a
Emil Gæde <s164414@student.dtu.dk>
2021-06-14 11:21:52 +0200

    Beautification of tcFunDec

--- GuardedCommands/TypeCheck.fs
+++ GuardedCommands/TypeCheck.fs
@@ -78,9 +78,9 @@ module TypeCheck =
                            //    Include every parameter
                            //    Include function itself
                            let ltenv' = List.fold tcGDec Map.empty decs
+                           let gtenv' = Map.add f (FTyp((List.choose (function VarDec(t,_) -> Some(t) | _ -> None)) decs,Some(t))) gtenv
                            // Check every return statement has type t
                            // Check stm is well-typed
-                           let gtenv' = Map.add f (FTyp((List.choose (function VarDec(t,_) -> Some(t) | _ -> None)) decs,Some(t))) gtenv
                            if not (tcFunDecBod gtenv' ltenv' t stm)
                            then failwith "type check: function body missing return statement"
                            gtenv'
