3896b20b975b8c4781bad44f7fb81ea66d7f3bbf
Troels Lund <trolund@gmail.com>
2021-06-14 20:34:51 +0200

    array declaration

--- GuardedCommands/Script.fsx
+++ GuardedCommands/Script.fsx
@@ -30,7 +30,7 @@ System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__;;
 
 // Basic tets ///////////////////////////////
 
-let testAndTree = parseFromFile "test_3/test_func_app_4.gc";;
+let testAndTree = parseFromFile "test_5/tc_arrayDec.gc";;
 
 let _ = tcP testAndTree;;
 
@@ -40,7 +40,7 @@ let _ = go testAndTree;;
 
 let _ = goTrace testAndTree;;
 
-let _ = exec "test_3/test_func_app_4.gc";;
+let _ = exec "test_5/tc_arrayDec.gc";;
 
 
 
