fa5ccbc1f0109c8869d69c9a503656ec9e2fd0fb
Troels Lund <trolund@gmail.com>
2021-06-15 19:52:50 +0200

    add tree lib

---
+++ GuardedCommands/PostScriptGenerator/Library.fs
@@ -0,0 +1,190 @@
+﻿namespace PostScriptGenerator
+
+open FPP1.TreeManager
+open System.IO
+open System.Text
+
+module Generator =
+
+    // global settings
+    let startX       = 0.0
+    let startY       = -20.0
+
+    let parentMargin = 10.0
+    let nodeHeight   = 20.0
+    let nodeWidth    = 50.0
+    let depthHeight  = 50.0
+    let depthMargin  = 16.0
+    let labelMargin  = 10.0
+
+    let psPre        = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
+    let showpage     = "showpage"
+    let stroke       = "stroke\n"
+
+
+    let positionX x pos =
+        match pos with
+        | 0.0 -> x
+        | _   -> x + pos * nodeWidth
+
+
+    let rec subtreeWidth ts =
+        match ts with 
+        | []                      -> 0.0
+        | Node ((_, pos), _)::[]  -> abs pos * nodeWidth
+        | Node ((_, pos), _)::ts' -> let (Node((_, pos'), _)) = List.last ts'
+                                     (abs pos + abs pos') * nodeWidth
+
+
+    let rec mostLabelSpaces ts = 
+        match ts with
+        | []                  -> 0.0
+        | Node((l, _), _)::ts -> let spaces = float (List.length ((string l).Split(' ') |> Array.toList))
+                                 max spaces (mostLabelSpaces ts)
+
+
+    let rec labelSpaceList curr next acc =
+        match curr, next with
+        | [], []               -> []
+        | Node(_, ts)::ts', [] -> labelSpaceList ts' ts (mostLabelSpaces ts)
+        | [], l                -> acc :: (labelSpaceList l [] 0.0)
+        | Node(_, ts)::ts', l  -> labelSpaceList ts' (l @ ts) (max acc (mostLabelSpaces ts))
+                              
+
+    let toPSslow t =
+        let moveto x y = 
+            string x + " " + string y + " moveto\n"
+         
+
+        let lineto x y = 
+            string x + " " + string y + " lineto\n"
+
+
+        let label l 
+            = "(" + string l + ") dup stringwidth pop 2 div neg 0 rmoveto show\n"
+
+            
+        let makeLabel (l:string) x y = 
+            let ls = l.Split(' ') |> Array.toList
+            let rec makeLabel' ls x y = 
+                match ls with
+                | []    -> ""
+                | l::ls -> moveto x y + label l + makeLabel' ls x (y - labelMargin)
+            makeLabel' ls x y
+
+                                       
+        let rec subtreeLines ts x y =
+            match ts with
+            | []                     -> ""
+            | Node ((_, pos), _)::ts -> let x' = positionX x pos 
+                                        moveto x' y + lineto x' (y - depthHeight) + subtreeLines ts x y
+
+                                        
+        let rec psTree t x y sl =
+            match t with
+            | Node ((l, _), []) -> makeLabel (string l) x y
+            | Node ((l, _), ts) -> let out = moveto x y
+                                   //let y = y - parentMargin
+                                   let out = out + (makeLabel l x y)
+                                   let (spaces, sls) =
+                                        match sl with
+                                        | [] -> 1.0 , []
+                                        | h :: slt -> h , slt
+                                   let y = y - spaces * labelMargin
+                                   let out = out + moveto x y
+                                   let y = y - nodeHeight
+                                   let out = out + lineto x y
+                                   let lineWidth = subtreeWidth ts
+                                   let halfLineWidth = lineWidth / 2.0
+                                   let x = x - halfLineWidth
+                                   let out = out + moveto x y
+                                   let x = x + lineWidth
+                                   let out = out + lineto x y + stroke
+                                   let x = x - halfLineWidth
+                                   let out = out + subtreeLines ts x y + psSubtrees ts x (y - depthHeight - depthMargin) sls
+                                   out
+        and psSubtrees ts x y sl =
+            match ts with
+            | []     -> ""
+            | t::ts' -> let (Node((_, pos), _)) = t
+                        let x' = positionX x pos
+                        psTree t x' y sl + psSubtrees ts' x y sl
+                           
+        psPre + psTree t startX startY (labelSpaceList [t] [] 0.0) + stroke + showpage
+
+
+    let toPSfast t =
+        let moveto x y =
+            String.concat " " [ string x; string y; "moveto\n" ]
+
+
+        let lineto x y =
+            String.concat " " [ string x; string y; "lineto\n" ]
+
+
+        let label l = 
+            String.concat "" [ "("; string l; ") dup stringwidth pop 2 div neg 0 rmoveto show\n" ]
+
+
+        let makeLabel (l:string) x y = 
+            let ls = l.Split(' ') |> Array.toList
+            let rec makeLabel' ls x y = 
+                match ls with
+                | []    -> ""
+                | l::ls -> String.concat "" [moveto x y; label l; makeLabel' ls x (y - labelMargin)]
+            makeLabel' ls x y
+           
+                                       
+        let rec subtreeLines ts x y =
+            match ts with
+            | []                     -> ""
+            | Node ((_, pos), _)::ts -> let x' = positionX x pos 
+                                        String.concat "" [moveto x' y; lineto x' (y - depthHeight); subtreeLines ts x y]
+   
+
+        let rec psTree t x y sl =
+            match t with
+            | Node((l, _), []) -> makeLabel (string l) x y
+            | Node((l, _), ts) ->   let sb = new StringBuilder()
+                                    sb.Append (moveto x y) |> ignore
+                                    //let y = y - parentMargin
+                                    sb.Append (makeLabel l x y)|> ignore
+                                    let (spaces, sls) =
+                                        match sl with
+                                        | [] -> 1.0 , []
+                                        | h :: slt -> h , slt
+                                    let y = y - spaces * labelMargin
+                                    sb.Append (moveto x y) |> ignore
+                                    let y = y - nodeHeight
+                                    sb.Append (lineto x y) |> ignore
+                                    let lineWidth = subtreeWidth ts
+                                    let halfLineWidth = lineWidth / 2.0
+                                    let x = x - halfLineWidth
+                                    sb.Append (moveto x y) |> ignore
+                                    let x = x + lineWidth
+                                    sb.Append (lineto x y) |> ignore
+                                    sb.Append (stroke) |> ignore
+                                    let x = x - halfLineWidth
+                                    sb.Append (subtreeLines ts x y) |> ignore
+                                    sb.Append (psSubtrees ts x (y - depthHeight - depthMargin) sls : string) |> ignore
+                                    sb.ToString()
+        and psSubtrees ts x y sl =
+            match ts with 
+            | []     -> ""
+            | t::ts' -> let (Node((_, pos), _)) = t
+                        let x' = positionX x pos
+                        String.concat "" [psTree t x' y sl; psSubtrees ts' x y sl]
+                           
+        String.concat "" [psPre; psTree t startX startY (labelSpaceList [t] [] 0.0); stroke; showpage]
+
+
+    let writeToFile n d =
+        File.WriteAllText("../output/" + n + ".ps", d)
+
+
+    let treeToFile n t =
+        writeToFile n (design t |> toPSfast)
+
+
+    let posTreeToFile n t =
+        writeToFile n (toPSfast t)
---
+++ GuardedCommands/TreeManager/Library.fs
@@ -0,0 +1,168 @@
+﻿namespace Tree
+
+open GuardedCommands.Frontend.AST
+
+module TreeManager =
+
+    type 'a Tree = Node of 'a * ('a Tree list)
+    type Extent = (float * float) list
+
+    let rec same' x y =
+        match x,y with
+        | Node((_,x'),[]),Node((_,y'),[]) when x'=y'-> true
+        | Node((_,x'),xs),Node((_,y'),ys) when x'=y' -> List.length xs = List.length ys && List.forall2 same' xs ys
+        | _,_ -> false
+    let rec same (Node(_,x)) (Node(_,y)) = List.length x = List.length y && List.forall2 same' x y
+
+
+    let rec similar x y =
+        match x,y with
+        | Node(_,[]), Node(_,[]) -> true
+        | Node(_,xs),Node(_,ys) -> List.length xs = List.length ys && List.forall2 similar xs ys
+
+
+    let movetree (Node ((label, x), subtrees), x': float) =
+        Node((label, x + x'), subtrees)
+
+
+    let moveextent (e, x) =
+        List.map (fun (p, q) -> (p + x, q + x)) e
+
+
+    let rec merge ps qs =
+        match (ps, qs) with
+        | ([], qs)                     -> qs
+        | (ps, [])                     -> ps
+        | ((p, _) :: ps, (_, q) :: qs) -> (p, q) :: merge ps qs
+
+
+    let mergelist es = 
+        List.fold merge [] es
+
+
+    let rmax (p: float, q: float) = 
+        if p > q then p else q
+
+
+    let rec fit =
+        function
+        | ((_, p) :: ps), ((q, _) :: qs) -> rmax (fit (ps, qs), p - q + 1.0)
+        | _                              -> 0.0
+
+
+    let fitlistl es =
+        let rec fitlistl' acc =
+            function
+            | []      -> []
+            | e :: es ->
+                let x = fit (acc, e)
+                x :: fitlistl' (merge acc (moveextent (e, x))) es
+        fitlistl' [] es
+
+
+    // val flipextent : Extent -> Extent = map (fn (p,q) => (~q,~p))
+    let flipextent e =
+        List.map (fun (p, q) -> (-q, -p)) e
+
+
+    // val fitlistr = rev o map ~ o fitlistl o map flipextent o rev
+    // let fitlistr (es: Extent list) =
+    //    List.rev (List.map (-) (fitlistl (List.map flipextent (List.rev es))));;
+    let fitlistr (es: Extent list) =
+        List.rev es
+        |> List.map flipextent
+        |> fitlistl
+        |> List.map (fun e -> -e)
+        |> List.rev
+
+
+    let mean (x, y) = 
+        (x + y) / 2.0
+
+
+    let fitlist es =
+        List.map mean (List.zip (fitlistl es) (fitlistr es))
+
+
+    let design tree =
+        let rec design' (Node (label, subtrees)) =
+            let (trees, extents) = List.unzip (List.map design' subtrees)
+            let positions = fitlist extents
+            let ptrees =
+                List.map movetree (List.zip trees positions)
+            let pextents =
+                List.map moveextent (List.zip extents positions)
+            let resultextent = (0.0, 0.0) :: mergelist pextents
+            let resulttree = Node((label, 0.0), ptrees)
+            (resulttree, resultextent)
+        fst (design' tree)
+
+
+    let rec parseExp e =
+        match e with
+        | N n           -> Node("Int " + string n, [])
+        | B b           -> Node("Bool " + string b, [])
+        | Access a      -> parseAccess a
+        | Addr a        -> parseAccess a
+        | Apply (s, es) -> Node("Apply " + s, parseExpList es)
+     and parseExpList es =
+        List.map parseExp es
+     and parseAccess a =
+        match a with
+        | AVar s        -> Node("Var \"" + s + "\"", [])
+        | AIndex (a, e) -> Node("Index", [parseAccess a; parseExp e])
+        | ADeref e      -> Node("Deref", [parseExp e])
+    and parseAccessList al = 
+        List.map parseAccess al
+
+    let rec parseTyp t =
+        match t with
+        | ITyp           -> Node("IntTyp", [])
+        | BTyp           -> Node("BoolTyp", [])
+        | ATyp (t, opt)  -> match opt with
+                            | Some(i) -> Node("Array", [parseTyp t] @ [Node (string i, [])])
+                            | None    -> Node("Array", [parseTyp t])
+        | PTyp t         -> Node ("Pointer", [parseTyp t])
+        | FTyp (ts, opt) -> match opt with
+                            | Some(t) -> Node("Function", parseTypList ts @ [parseTyp t])
+                            | None    -> Node("Function", parseTypList ts)
+    and parseTypList ts =
+        List.map parseTyp ts
+
+    let rec parseDec d =
+        match d with
+        | VarDec (t, s)            -> Node ("VarDec", [Node (s, []); parseTyp t])
+        | FunDec (opt, s, ds, stm) -> match opt with
+                                      | Some(t) -> let nodes = [Node(s, [parseTyp t] @ parseDecList ds @ [parseStm stm])]
+                                                   Node("FunDec", nodes)
+                                      | None    -> let nodes = [Node(s, parseDecList ds @ [parseStm stm])]
+                                                   Node("FunDec", nodes)
+    and parseDecList ds = 
+        List.map parseDec ds
+    and parseStm s =
+        match s with
+        | PrintLn e        -> Node("PrintLn", [parseExp e])
+        | Ass (a, e)       -> Node("Ass", parseAccessList a @ parseExpList e)
+        | Return opt       -> match opt with
+                              | Some(e) -> Node("Return", [parseExp e])
+                              | None    -> Node("Return", [])
+        | Alt gc           -> Node("Alt", [parseGc gc])
+        | Do gc            -> Node("Do", [parseGc gc])
+        | Block (ds, ss)   -> Node("Block", parseDecList ds @ parseStmList ss)
+        | Call (s, es)     -> Node("Call " + s, parseExpList es)
+    and parseStmList ss =
+        List.map parseStm ss
+    and parseGc gc =
+        match gc with
+        | GC ([])            -> failwith "Empty GuardedCommand!"
+        | GC (((e, ss)::[])) -> Node("Guarded Command", [parseExp e] @ parseStmList ss)
+        | GC ((e, ss)::gs)   -> Node("Guarded Command", [parseExp e] @ parseStmList ss @ [parseGcList gs])
+    and parseGcList gs =
+        parseGc (GC gs)
+
+    let parseProgram p =
+        match p with
+        | P (ds, ss) -> Node ("Program", parseDecList ds @ parseStmList ss)                     
+
+    let toGeneralTree p =
+        parseProgram p
