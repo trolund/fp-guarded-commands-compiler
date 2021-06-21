01de2e392bf14e8711ca0a1d25414d84e006b544
Emil Gæde <s164414@student.dtu.dk>
2021-06-21 15:27:36 +0200

    Added tests 6-E to script

--- GuardedCommands/Script.fsx
+++ GuardedCommands/Script.fsx
@@ -120,31 +120,49 @@ let _ = exec "test/Ex3.gc";;
 // Test of programs covered by the first task (Section 3.7):
 List.iter exec ["test/Ex1.gc"; "test/Ex2.gc";"test/Ex3.gc"; "test/Ex4.gc"; "test/Ex5.gc"; "test/Ex6.gc"; "test/Skip.gc"];;
 
+// Own tests
+List.iter exec ["test_3/tc_alternative_2.gc"; "test_3/tc_alternative_1.gc"; "test_3/tc_alternative_stop.gc"; "test_3/tc_alternative.gc"; "test_3/tc_and.gc"; "test_3/tc_division.gc"; "test_3/tc_equal.gc"; "test_3/tc_greaterthan.gc"; "test_3/tc_greaterthanequals.gc"; "test_3/tc_lessthan.gc"; "test_3/tc_minus.gc"; "test_3/tc_modulus.gc"; "test_3/tc_negation_2.gc"; "test_3/tc_negation.gc"; "test_3/tc_notequal.gc"; "test_3/tc_or.gc"; "test_3/tc_plus.gc"; "test_3/tc_repetition.gc"; "test_3/tc_times.gc"];;
+List.iter execOpt ["test_3/tc_alternative_2.gc"; "test_3/tc_alternative_1.gc"; "test_3/tc_alternative_stop.gc"; "test_3/tc_alternative.gc"; "test_3/tc_and.gc"; "test_3/tc_division.gc"; "test_3/tc_equal.gc"; "test_3/tc_greaterthan.gc"; "test_3/tc_greaterthanequals.gc"; "test_3/tc_lessthan.gc"; "test_3/tc_minus.gc"; "test_3/tc_modulus.gc"; "test_3/tc_negation_2.gc"; "test_3/tc_negation.gc"; "test_3/tc_notequal.gc"; "test_3/tc_or.gc"; "test_3/tc_plus.gc"; "test_3/tc_repetition.gc"; "test_3/tc_times.gc"];;
+
+
 // Test of programs covered by the second task (Section 4.3):
 List.iter exec ["test/Ex7.gc"; "test/fact.gc"; "test/factRec.gc"; "test/factCBV.gc"];;
+
+// Own tests
+List.iter exec ["test_4/tc_func_app_1.gc"; "test_4/tc_func_app_2.gc"; "test_4/tc_func_app_3.gc"; "test_4/tc_func_app_4.gc"; "test_4/tc_func_app_5.gc"; "test_4/tc_func_app_6.gc"; "test_4/tc_func_app.gc"; "test_4/tc_func_dec_1.gc"; "test_4/tc_func_dec_2.gc"; "test_4/tc_func_dec_3.gc"; "test_4/tc_func_dec_4.gc"; "test_4/tc_func_dec_alternative.gc"; "test_4/tc_func_global_dec_ass_1.gc"; "test_4/tc_func_local_dec_1.gc"; "test_4/tc_func_local_dec_2.gc"; "test_4/tc_func_local_dec_3.gc"; "test_4/tc_func_local_dec_app_1.gc"; "test_4/tc_func_local_dec_app_2.gc"; "test_4/tc_func_local_dec_app_3.gc"; "test_4/tc_local_dec_non_func_correct.gc"; "test_4/tc_local_dec.gc"];;
+List.iter execOpt ["test_4/tc_func_app_1.gc"; "test_4/tc_func_app_2.gc"; "test_4/tc_func_app_3.gc"; "test_4/tc_func_app_4.gc"; "test_4/tc_func_app_5.gc"; "test_4/tc_func_app_6.gc"; "test_4/tc_func_app.gc"; "test_4/tc_func_dec_1.gc"; "test_4/tc_func_dec_2.gc"; "test_4/tc_func_dec_3.gc"; "test_4/tc_func_dec_4.gc"; "test_4/tc_func_dec_alternative.gc"; "test_4/tc_func_global_dec_ass_1.gc"; "test_4/tc_func_local_dec_1.gc"; "test_4/tc_func_local_dec_2.gc"; "test_4/tc_func_local_dec_3.gc"; "test_4/tc_func_local_dec_app_1.gc"; "test_4/tc_func_local_dec_app_2.gc"; "test_4/tc_func_local_dec_app_3.gc"; "test_4/tc_local_dec_non_func_correct.gc"; "test_4/tc_local_dec.gc"];;
+
 
 // Test of programs covered by the fourth task (Section 5.4):
 List.iter exec ["test/A0.gc"; "test/A1.gc"; "test/A2.gc"; "test/A3.gc"];;
 
+// Own tests
+List.iter exec ["test_5/tc_array_dec.gc"; "test_5/tc_array_index_ass_op.gc"; "test_5/tc_array_index_ass.gc"; "test_5/tc_array_index.gc"; "test_5/tc_func_taking_array.gc"];;
+List.iter execOpt ["test_5/tc_array_dec.gc"; "test_5/tc_array_index_ass_op.gc"; "test_5/tc_array_index_ass.gc"; "test_5/tc_array_index.gc"; "test_5/tc_func_taking_array.gc"];;
+
 // Test of programs covered by the fifth task (Section 6.1):
 List.iter exec ["test/A4.gc"; "test/Swap.gc"; "test/QuickSortV1.gc"];;
+
+// Own test of programs using multiple assignment
+List.iter exec ["test_6/tc_ma_eval_before_assign.gc";"test_6/tc_ma_eval_before_loc_before_ass.gc"; "test_6/tc_ma_eval_order.gc"; "test_6/tc_ma_loc_order.gc"; "test_6/tc_ma_overwriting_ass.gc"; "test_6/tc_ma_proc.gc"; "test_6/tc_ma_side_effects_2.gc"; "test_6/tc_ma_side_effects.gc"; "test_6/tc_ma_simple.gc"];;
+List.iter execOpt ["test_6/tc_ma_eval_before_assign.gc";"test_6/tc_ma_eval_before_loc_before_ass.gc"; "test_6/tc_ma_eval_order.gc"; "test_6/tc_ma_loc_order.gc"; "test_6/tc_ma_overwriting_ass.gc"; "test_6/tc_ma_proc.gc"; "test_6/tc_ma_side_effects_2.gc"; "test_6/tc_ma_side_effects.gc"; "test_6/tc_ma_simple.gc"];;
+
+// Own test of programs using procedures
+List.iter exec ["test_7/tc_proc_call.gc"; "test_7/tc_proc_call_1.gc"; "test_7/tc_proc_call_2.gc"; "test_7/tc_proc_call_3.gc"; "test_7/tc_proc_call_4.gc"; "test_7/tc_proc_call_no_return.gc"; "test_7/tc_proc_dec_1.gc"; "test_7/tc_proc_dec_2.gc"; "test_7/tc_proc_dec_3.gc"; "test_7/tc_proc_dec_4.gc"];;
+List.iter execOpt ["test_7/tc_proc_call.gc"; "test_7/tc_proc_call_1.gc"; "test_7/tc_proc_call_2.gc"; "test_7/tc_proc_call_3.gc"; "test_7/tc_proc_call_4.gc"; "test_7/tc_proc_call_no_return.gc"; "test_7/tc_proc_dec_1.gc"; "test_7/tc_proc_dec_2.gc"; "test_7/tc_proc_dec_3.gc"; "test_7/tc_proc_dec_4.gc"];;
 
 // Test of programs covered by the fifth task (Section 7.4):
 List.iter exec ["test/par1.gc"; "test/factImpPTyp.gc"; "test/QuickSortV2.gc"];;
 
+// Own test of programs using pointers
+List.iter exec ["test_8/tc_p_simple.gc"; "test_8/tc_p_to_p.gc"; "test_8/tc_p_to_p_2.gc"; "test_8/tc_p_to_p_3.gc"];;
+List.iter execOpt ["test_8/tc_p_simple.gc"; "test_8/tc_p_to_p.gc"; "test_8/tc_p_to_p_2.gc"; "test_8/tc_p_to_p_3.gc"];;
+
 // Test of programs covered by the fifth task using optimized compilation (Section 8.2):
 List.iter execOpt ["test/par1.gc"; "test/factImpPTyp.gc"; "test/QuickSortV2.gc"];;
 
-// OWN TESTS
+// Own test of extensions
+List.iter exec ["test_extensions/tc_A0_comment.gc"; "test_extensions/tc_cond_1.gc"; "test_extensions/tc_cond_2.gc"; "test_extensions/tc_cond_3.gc"; "test_extensions/tc_cond_4.gc"; "test_extensions/tc_Ex11_comment.gc"; "test_extensions/tc_preop_1.gc"; "test_extensions/tc_preop_2.gc"];;
+List.iter execOpt ["test_extensions/tc_A0_comment.gc"; "test_extensions/tc_cond_1.gc"; "test_extensions/tc_cond_2.gc"; "test_extensions/tc_cond_3.gc"; "test_extensions/tc_cond_4.gc"; "test_extensions/tc_Ex11_comment.gc"; "test_extensions/tc_preop_1.gc"; "test_extensions/tc_preop_2.gc"];;
 
-// Test of programs covered test_3
-List.iter exec ["test_3/tc_alternative_2.gc"; "test_3/tc_alternative_1.gc"; "test_3/tc_alternative_stop.gc"; "test_3/tc_alternative.gc"; "test_3/tc_and.gc"; "test_3/tc_division.gc"; "test_3/tc_equal.gc"; "test_3/tc_greaterthan.gc"; "test_3/tc_greaterthanequals.gc"; "test_3/tc_lessthan.gc"; "test_3/tc_minus.gc"; "test_3/tc_modulus.gc"; "test_3/tc_negation_2.gc"; "test_3/tc_negation.gc"; "test_3/tc_notequal.gc"; "test_3/tc_or.gc"; "test_3/tc_plus.gc"; "test_3/tc_repetition.gc"; "test_3/tc_times.gc"];;
-List.iter execOpt ["test_3/tc_alternative_2.gc"; "test_3/tc_alternative_1.gc"; "test_3/tc_alternative_stop.gc"; "test_3/tc_alternative.gc"; "test_3/tc_and.gc"; "test_3/tc_division.gc"; "test_3/tc_equal.gc"; "test_3/tc_greaterthan.gc"; "test_3/tc_greaterthanequals.gc"; "test_3/tc_lessthan.gc"; "test_3/tc_minus.gc"; "test_3/tc_modulus.gc"; "test_3/tc_negation_2.gc"; "test_3/tc_negation.gc"; "test_3/tc_notequal.gc"; "test_3/tc_or.gc"; "test_3/tc_plus.gc"; "test_3/tc_repetition.gc"; "test_3/tc_times.gc"];;
 
-// Test of programs covered test_4
-List.iter exec ["test_4/tc_func_app_1.gc"; "test_4/tc_func_app_2.gc"; "test_4/tc_func_app_3.gc"; "test_4/tc_func_app_4.gc"; "test_4/tc_func_app_5.gc"; "test_4/tc_func_app_6.gc"; "test_4/tc_func_app.gc"; "test_4/tc_func_dec_1.gc"; "test_4/tc_func_dec_2.gc"; "test_4/tc_func_dec_3.gc"; "test_4/tc_func_dec_4.gc"; "test_4/tc_func_dec_alternative.gc"; "test_4/tc_func_global_dec_ass_1.gc"; "test_4/tc_func_local_dec_1.gc"; "test_4/tc_func_local_dec_2.gc"; "test_4/tc_func_local_dec_3.gc"; "test_4/tc_func_local_dec_app_1.gc"; "test_4/tc_func_local_dec_app_2.gc"; "test_4/tc_func_local_dec_app_3.gc"; "test_4/tc_local_dec_non_func_correct.gc"; "test_4/tc_local_dec.gc"];;
-List.iter execOpt ["test_4/tc_func_app_1.gc"; "test_4/tc_func_app_2.gc"; "test_4/tc_func_app_3.gc"; "test_4/tc_func_app_4.gc"; "test_4/tc_func_app_5.gc"; "test_4/tc_func_app_6.gc"; "test_4/tc_func_app.gc"; "test_4/tc_func_dec_1.gc"; "test_4/tc_func_dec_2.gc"; "test_4/tc_func_dec_3.gc"; "test_4/tc_func_dec_4.gc"; "test_4/tc_func_dec_alternative.gc"; "test_4/tc_func_global_dec_ass_1.gc"; "test_4/tc_func_local_dec_1.gc"; "test_4/tc_func_local_dec_2.gc"; "test_4/tc_func_local_dec_3.gc"; "test_4/tc_func_local_dec_app_1.gc"; "test_4/tc_func_local_dec_app_2.gc"; "test_4/tc_func_local_dec_app_3.gc"; "test_4/tc_local_dec_non_func_correct.gc"; "test_4/tc_local_dec.gc"];;
-
-// Test of programs covered test_5
-List.iter exec ["test_5/tc_array_dec.gc"; "test_5/tc_array_index_ass_op.gc"; "test_5/tc_array_index_ass.gc"; "test_5/tc_array_index.gc"; "test_5/tc_func_taking_array.gc"];;
-List.iter execOpt ["test_5/tc_array_dec.gc"; "test_5/tc_array_index_ass_op.gc"; "test_5/tc_array_index_ass.gc"; "test_5/tc_array_index.gc"; "test_5/tc_func_taking_array.gc"];;
